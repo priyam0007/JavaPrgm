0859de623ada05f18c9c80351747ee1cc6ad7953
Priya <Priyamadiwalar0007@gmail.com>
2022-02-03 19:52:16 +0530

    added: addition.Java

---
+++ src/com/java/Addition.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		int add=num1+num2;
+		 System.out.println("the addition is "+add);
+		// TODO Auto-generated method stub
+
+	}
+
+}
